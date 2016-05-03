c1b600bc630077485bbef89c36ab3e2386955873
Aryeh Alexander <alalexan@andrew.cmu.edu>
2016-05-03 14:35:32 -0400

    Hopefully everything is clean now...

--- src/test.cpp
+++ src/test.cpp
@@ -7,7 +7,7 @@
 #include "../include/CycleTimer.h"
 #include <stdio.h>
 #include <unistd.h>
-
+#include <random>
 //The tester file for operations
 using namespace std;
 
@@ -22,9 +22,17 @@ typedef struct cmd{
 
 } cmd;
 
+typedef struct trace{
+	vector<cmd> cmdlist;
+	int nIns;
+	int nRem;
+	int nFin;
+} trace;
+
+
 void println(string str);
-vector<cmd> parseFile(ifstream &ifs);
-void runCmdList(vector<cmd> &cmdlist , List &l);
+void parseFile(ifstream &ifs, trace &tr);
+void runCmdList(trace &cmdlist , List &l);
 
 //TODO get oparg working
 int main(int argc , char *argv[]){
@@ -52,9 +60,11 @@ int main(int argc , char *argv[]){
 	}
 
 	//Run Stuff
-	vector<cmd> cmdlist = parseFile(ifs);
+	trace tr;
 	List testList;
-	runCmdList(cmdlist, testList);
+
+	parseFile(ifs , tr);
+	runCmdList(tr, testList);
 	println(testList.printlist());	
 
 	println("DONE WITH TESTING");
@@ -62,32 +72,44 @@ int main(int argc , char *argv[]){
 }
 
 
-vector<cmd> parseFile(ifstream &ifs){
+void parseFile(ifstream &ifs , trace &tr){
 		
 	// CMD INT
 	Op op;
 	int val;
 	string s;
 	vector<cmd> cmdlist; 
+	
+	int nIns = 0;
+	int nRem = 0;
+	int nFin = 0;
+
+
 	for( string line; getline( ifs, line ); ) {
 	  stringstream ss(line);
 	  ss >> s;
 	  if(s == "ins" || s == "INS"){
 	  	op = INS;
+		nIns++;
 	  }
 	  else if(s == "rem" || s == "REM"){
 	  	op = REM;
+		nRem++;
 	  }
 	  else if(s == "fin" || s=="FIN"){
-		  op = FIN;
+		op = FIN;
+		nFin++;
 	  }
 	  // SKIP OVER OTHERWISE
 	  ss >> s;
 	  val = atoi(s.c_str()); 
 	  cmd plop(op , val , 0);
 	  cmdlist.push_back(plop);
-	}	    
-	return cmdlist;
+	}
+	tr.nIns = nIns;
+	tr.nRem = nRem;
+	tr.nFin = nFin;
+	tr.cmdlist = cmdlist;
 }
 
 void println(string str){
@@ -96,13 +118,12 @@ void println(string str){
 
 //What about multiple threads yo?
 //TODO Find nice way to 
-void runCmdList(vector<cmd> &cmdlist , List &l){
-	
+void runCmdList(trace &tr , List &l){
 	//Maybe make a log here???
 	double start;
 	double end;
 
-	for(vector<cmd>::iterator it = cmdlist.begin(); it != cmdlist.end(); it++){
+	for(vector<cmd>::iterator it = tr.cmdlist.begin(); it != tr.cmdlist.end(); it++){
 	  cmd & c  = *it;
 	  switch( c.op){
 		  case REM:
@@ -128,16 +149,58 @@ void runCmdList(vector<cmd> &cmdlist , List &l){
 	println("Done with commands");
 	println("Doing calculations");
 	double totaltime = 0;;
-	for(vector<cmd>::iterator it = cmdlist.begin(); it != cmdlist.end(); it++){
+	for(vector<cmd>::iterator it = tr.cmdlist.begin(); it != tr.cmdlist.end(); it++){
 		cmd & c = *it; 
 		totaltime += c.time;
 	}
 
-	//STATS
+	//STATS ->> move to another function.
 	println("Total time taken");
 	cout << totaltime << endl;
 	println("Average time taken");
+	cout << totaltime / ((double) tr.cmdlist.size()) << endl;
+	//STATS ->> move to another function.
 }
+
+void randTrace(trace &tr, int n, double top = 66.66f , double mid = 33.33f ){
+	random_device rd;
+	uniform_real_distribution<double> distribution(1, 100);
+	mt19937 engine(rd());
+	
+	Op op;
+	int val;
+	string s;
+	vector<cmd> cmdlist; 
+	
+	int nIns = 0;
+	int nRem = 0;
+	int nFin = 0;
+	
+	double yolo;
+	for(int i =0; i<n; i++){
+	  yolo = distribution(engine);
+	  if(yolo > top){
+	    op  = INS;
+	    nIns++;
+	  }
+	  else if(yolo > mid){
+	    op = REM;
+	    nRem++;
+	  }
+	  else {
+	    op = FIN;
+	    nFin++;
+	  }	
+	  cmd plop(op , (int)yolo , 0);
+	  cmdlist.push_back(plop);
+	}
+	tr.nIns = nIns;
+	tr.nRem = nRem;
+	tr.nFin = nFin;
+	tr.cmdlist = cmdlist;
+}
+
+
 
 
 
