a6f3afdf8c1cb3650a07112b718c906f87053e7b
Christtia Zheng <christtz@andrew.cmu.edu>
2016-05-03 16:10:51 -0400

    new stuff with template whatever

--- src/lists/sorted_list_seq.h
+++ src/lists/sorted_list_seq.h
@@ -1,37 +1,21 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <mylist.h>
 
 using namespace std;
 
-//Simple list node with ints, not embedded
-typedef struct listnode {
-	listnode *next;
-	int value;
-	listnode (int v = 0) : value(v) {};
-}listnode;
-
-class List{
+class SeqList : public List{
 	public:
 	void insert(int value);
 	void remove(int value);
 	bool find(int value);
-	string printlist();
-	List();
-	
-	private:
-	listnode *head;
-	listnode *tail;
+	//string printlist();
+	SeqList() : List(){ };
 };
 
-List::List(void){
-	head = new listnode();
-	tail = new listnode();
-	head->next = tail;
-}
-
-//Inserts a new node to sorted list (assume duplicates are allowed)
-void List::insert(int value){
+//Adds to front of the list
+void SeqList::insert(int value){
 	listnode *ptr = new listnode(value);
 	listnode *curr = head;
 	//Special case for head end
@@ -48,9 +32,8 @@ void List::insert(int value){
 	}
 }
 
-//Remove first occurrence of node with value 
-//Does nothing if value is not in the list
-void List::remove(int value){
+//First occurence
+void SeqList::remove(int value){
 	listnode *curr = head->next;
 	listnode *prev = head;
 	while(curr != tail && curr->value <= value) {
@@ -64,7 +47,7 @@ void List::remove(int value){
 	}
 }
 
-bool List::find(int value){
+bool SeqList::find(int value){
 	listnode *curr = head;
 	while(curr->next != tail && curr->next->value <= value){
 		if (curr->next->value == value) return true;
@@ -73,12 +56,4 @@ bool List::find(int value){
 	return false;
 }
 
-string List::printlist(){
-	listnode *ptr = head->next;
-	stringstream s;
-	while(ptr != tail){
-		s << ptr->value << " ";  
-		ptr = ptr->next;
-	}
-	return s.str();
-}
+
